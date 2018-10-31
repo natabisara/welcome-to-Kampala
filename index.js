d1201eaa425a23f2d04a8984fb93a82714d60241
natabisara <43807347+natabisara@users.noreply.github.com>
2018-10-31 17:51:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+    
+    alert(" WELCOME!!!!");
+    var FirstName
+    var LastName
+    console.log("sara");
+    console.log("Natabi");
+    prompt("First Name");
+    promt("Last Name");
+    if(FirstName==LastName){LOGIN};
+    
+
+       var Fruits="pineapple";
+        switch(Fruits){
+            case oranges:
+            alert("oranges are not arround")
+            break;
+            case mangoes:
+            alert("mangoes cost shs.100 each")
+            break;
+            default:
+            alert(" i enjoy all fruits");
+        }
+        console. log (Fruits);
+
+
+
+
+
